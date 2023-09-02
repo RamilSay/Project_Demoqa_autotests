55c4af06660d102b8a8516c4852cb9972c3cb9ed
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-02 22:28:23 +0700

    working

--- conftest.py
+++ conftest.py
@@ -3,9 +3,10 @@ from selene import browser
 
 
 @pytest.fixture(scope="function", autouse=True)
-def browser_management():
-    browser.config.timeout = 3.0
+def browser_setup():
     browser.config.base_url = "https://demoqa.com/automation-practice-form"
+    browser.config.window_width = "1920"
+    browser.config.window_height = "1080"
 
     yield
 
