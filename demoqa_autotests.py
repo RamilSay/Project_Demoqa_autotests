dee3e49a6f72380187f4b6a70809702e9fa5ca04
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-03 13:20:24 +0700

    working_6

--- demoqa_autotests.py
+++ demoqa_autotests.py
@@ -4,9 +4,9 @@ from selene import browser
 #Тестирование формы
 def testing_DemoQA():
     browser.open('https://demoqa.com/automation-practice-form')
-    browser.element('#firstName').type('Ramil')
-    browser.element('#lastName').type('Sayapov')
-    browser.element('#userEmail').type('ramilsayapov71@gmail.com')
+    browser.element('#firstName').type('Vasya')
+    browser.element('#lastName').type('Filin')
+    browser.element('#userEmail').type('fortest@gmail.com')
     browser.element('[for="gender-radio-1"]').click()
     browser.element('#userNumber').type('89995557777')
 
