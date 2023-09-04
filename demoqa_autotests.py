cfb7401894c45fea2b6197f0a256c4acf21ddc7a
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-04 22:08:53 +0700

    working_9

--- demoqa_autotests.py
+++ demoqa_autotests.py
@@ -2,9 +2,8 @@ import os
 from selene import browser, be, have
 
 
-
 def testing_DemoQA():
-#Values for testing
+    # Values for testing
     name = "Vasya"
     last_name = "Filin"
     e_mail = "fortest@gmail.com"
@@ -20,7 +19,7 @@ def testing_DemoQA():
     browser.element("[for='gender-radio-1']").click()
     browser.element("#userNumber").type("mobile")
 
-#date of birth
+    # date of birth
     browser.element("#dateOfBirthInput").click()
     browser.element(".react-datepicker__month-select").click()
     browser.element(".react-datepicker__month-select [value='6']").click()
@@ -28,24 +27,19 @@ def testing_DemoQA():
     browser.element(".react-datepicker__year-select [value='1999']").click()
     browser.element(".react-datepicker__day.react-datepicker__day--007").click()
 
-#subjects & hobbies
+    # subjects & hobbies
     browser.element("#subjectsInput").type("math").press_enter()
     browser.element("#subjectsInput").type("Physics").press_enter()
     browser.element("[for='hobbies-checkbox-1']").click()
     browser.element("[for='hobbies-checkbox-3']").click()
     browser.element("#uploadPicture").send_keys(os.path.abspath("/IMG_1.jpg"))
 
-#Adress
-    browser.element("#currentAddress").type("adress")
+    # Address
+    browser.element("#currentAddress").type("address")
     browser.element("#react-select-3-input").type("state").press_enter()
     browser.element("#react-select-4-input").type("city").press_enter()
     browser.element("#submit").press_enter()
 
-#asserts
-    browser.element(".form").should(have.text("VASYA"))
-    browser.element(".form").should(have.text("last_name"))
-
-
-
-
-
+    # asserts
+    browser.element(".table").should(have.text("Vasya"))
+    browser.element(".table").should(have.text("last_name"))
