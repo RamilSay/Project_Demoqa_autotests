20aa0eede18bab4fc432722c181d232a4b03bcd0
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-10-14 13:03:35 +0700

    work

---
+++ model/pages/registration_page.py
@@ -0,0 +1,7 @@
+from selene import browser
+
+class ReqistrationPage:
+    def __init__(self):
+        self.first_name = browser.element('#firstName')
+        self.last_name = browser.element('#lastName')
+        self.email = browser.element('#userEmail')
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from selene import browser
 
 @pytest.fixture(scope="function", autouse=True)
 def browser_setup():
+    browser.config.base_url = 'https://demoqa.com'
     browser.config.hold_driver_at_exit = True
     browser.config.window_width = 1920
     browser.config.window_height = 1080
