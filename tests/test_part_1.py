067e4720399db62a6e4fe36836751e11f2476672
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-10-22 22:19:56 +0700

    working

--- tests/test_part_1.py
+++ tests/test_part_1.py
@@ -5,14 +5,13 @@ def test_student_registration_form():
     registration_page = RegistrationPage()
     registration_page.open()
 
-
     # Values for testing
-    #hobbies = "Sports, Music"
-    #address = "1-street, 7-house, 7-apartment"
-    #state = "NCR"
-    #city = "Delhi"
+    # hobbies = "Sports, Music"
+    # address = "1-street, 7-house, 7-apartment"
+    # state = "NCR"
+    # city = "Delhi"
 
-    #WHEN
+    # WHEN
     registration_page.fill_first_name('Natali')
     registration_page.fill_last_name('Ivanova')
     registration_page.fill_email('fortest@gmail.com')
@@ -28,31 +27,26 @@ def test_student_registration_form():
     registration_page.hobbie_sport.click()
     registration_page.hobbie_music.click()
 
+    registration_page.upload_picture()
 
 
-    # subjects & hobbies
-
-
-    #browser.element("#uploadPicture").send_keys(os.path.abspath("resource/IMG_1.jpg"))
-    #
-    ## address
-    #browser.element("#currentAddress").type(address)
-    #browser.element("#react-select-3-input").type(state).press_enter()
-    #browser.element("#react-select-4-input").type(city).press_enter()
-    #browser.element("#submit").press_enter()
+    # browser.element("#currentAddress").type(address)
+    # browser.element("#react-select-3-input").type(state).press_enter()
+    # browser.element("#react-select-4-input").type(city).press_enter()
+    # browser.element("#submit").press_enter()
 
     ## asserts
-    #browser.element(".table").should(have.text(name))
-    #browser.element(".table").should(have.text(last_name))
-    #browser.element(".table").should(have.text(e_mail))
-    #browser.element("#gender-radio-1").should(have.value("Male"))
-    #browser.element(".table").should(have.text(mobile))
-    #browser.element("#dateOfBirthInput").should(have.value("07 Jul 1999"))
-    #browser.element(".table").should(have.text("Maths"))
-    #browser.element(".table").should(have.text("Physics"))
-    #browser.element(".table").should(have.text(hobbies))
-    #browser.element(".table").should(have.text("IMG_1.jpg"))
-    #browser.element(".table").should(have.text(address))
-    #browser.element(".table").should(have.text(state))
-    #browser.element(".table").should(have.text(city))
+    # browser.element(".table").should(have.text(name))
+    # browser.element(".table").should(have.text(last_name))
+    # browser.element(".table").should(have.text(e_mail))
+    # browser.element("#gender-radio-1").should(have.value("Male"))
+    # browser.element(".table").should(have.text(mobile))
+    # browser.element("#dateOfBirthInput").should(have.value("07 Jul 1999"))
+    # browser.element(".table").should(have.text("Maths"))
+    # browser.element(".table").should(have.text("Physics"))
+    # browser.element(".table").should(have.text(hobbies))
+    # browser.element(".table").should(have.text("IMG_1.jpg"))
+    # browser.element(".table").should(have.text(address))
+    # browser.element(".table").should(have.text(state))
+    # browser.element(".table").should(have.text(city))
 
