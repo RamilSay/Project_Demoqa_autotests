778620597b9c355ab0998fa208dcffa23731705a
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-12-06 23:39:20 +0700

    allure work

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,22 @@
 import pytest
 
-from selene import Browser, Config
+from selene import browser, Config
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
+from utils import attach
 
 @pytest.fixture(scope='function')
 def setup_browser():
     browser.config.base_url = 'https://demoqa.com'
-    #browser.config.hold_driver_at_exit = True
+    # browser.config.hold_driver_at_exit = True
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+    browser_version = '100.0'
     options = Options()
     selenoid_capabilities = {
         'browserName': 'chrome',
-        'browserVersion': '100.0',
+        'browserVersion': browser_version,
         'selenoid:options': {
             'enableVNC': True,
             'enableVideo': True
@@ -27,8 +28,13 @@ def setup_browser():
         options=options
     )
 
-    browser = Browser(Config(driver))
+    browser.config.driver = driver
 
-    yield
+    yield driver
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
